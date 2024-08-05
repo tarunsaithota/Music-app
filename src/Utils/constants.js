f0bbfcb0815ab1e8d546d5f7fba19e9fa67160fe
Tarun <thotatarun96@gmail.com>
2024-08-05 09:32:39 -0700

    update env file

--- src/Utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" + GOOGLE_API_KEY
-
-export const GOOGLE_API_KEY = AIzaSyDwMk2NntWVPtSbmYpY5FPU8q68SrkHXmA;
