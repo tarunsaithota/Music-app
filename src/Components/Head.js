c2da0c9495016daf514b8323e7d7397a0589e844
Tarun <thotatarun96@gmail.com>
2024-08-15 14:29:15 -0700

    Fix page responsiveness

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -7,18 +7,18 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Utils/firebase";
 import { signOut } from "firebase/auth";
 import { addUser, removeUser } from "../Utils/userSlice";
-import Banner from "./Banner";
 import LOGO from "../Utils/logo_img.png";
 import { addSearchSuggestions } from "../Utils/searchSuggestionSlice";
 
 const Head = () => {
   const [showSuggestions, setShowSuggestions] = useState(false);
-  const [searchData, setSearchData] = useState("")
+  const [searchData, setSearchData] = useState("");
   const [storeSuggestions, setStoreSuggestions] = useState([]);
-  const searchCache = useSelector((store)=>store.search)
+  const searchCache = useSelector((store) => store.search);
+  const suggestionBoxRef = useRef(null);
   const handleSearchDataChange = (e) => {
     setSearchData(e.target.value);
-  }
+  };
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userDetails = useSelector((store) => store.user);
@@ -41,27 +41,34 @@ const Head = () => {
     dispatch(fetchSearchSongs(data.data.results));
   };
   const handleSearchSuggestion = async (s) => {
-    const result = await fetch(
-      "https://saavn.dev/api/search/songs?query=" + s
-    );
+    const result = await fetch("https://saavn.dev/api/search/songs?query=" + s);
     const data = await result.json();
-    console.log(data.data.results);
     dispatch(fetchSearchSongs(data.data.results));
     setShowSuggestions(false);
   };
-  const getSearchSuggestions = async() => {
-    const response = await fetch(`https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`+searchData);
+  const getSearchSuggestions = async () => {
+    const response = await fetch(
+      `https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=` +
+        searchData
+    );
     const jsonData = await response.json();
     setStoreSuggestions(jsonData[1]);
-    dispatch(addSearchSuggestions({
-      [searchData]:jsonData[1]
-    }))
-  }
-  const handleOnBlur =() => {
-    setTimeout(()=>{
-      setShowSuggestions(false);
-    },200) 
-  }
+    dispatch(
+      addSearchSuggestions({
+        [searchData]: jsonData[1],
+      })
+    );
+  };
+  const handleOnBlur = (e) => {
+    if (
+      suggestionBoxRef.current &&
+      suggestionBoxRef.current.contains(e.relatedTarget)
+    ) {
+      return;
+    }
+    setShowSuggestions(false);
+  };
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -74,18 +81,18 @@ const Head = () => {
       }
     });
   }, []);
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
-      if(searchCache[searchData]){
-        setStoreSuggestions(searchCache[searchData])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchData]) {
+        setStoreSuggestions(searchCache[searchData]);
+      } else {
+        getSearchSuggestions();
       }
-      else {
-        getSearchSuggestions()
-      }}, 200)
+    }, 200);
     return () => {
       clearTimeout(timer);
-    }
-  },[searchData]);
+    };
+  }, [searchData]);
   return (
     <>
       <div className="grid grid-flow-col p-3 md:p-2 md:pt-3 justify-between bg-black text-white">
@@ -102,27 +109,41 @@ const Head = () => {
         {userDetails && (
           <>
             <div className="col-span-4 px-10">
-              <div><input
-                className="w-1/2 border border-gray-700 md:p-1 pl-2 md:pl-4 rounded-l-full text-black "
-                type="text"
-                placeholder="Search"
-                value={searchData}
-                onChange={handleSearchDataChange}
-                onFocus={()=>setShowSuggestions(true)}
-                onBlur={handleOnBlur}
-              />
-              <button
-                className="border border-gray-700 md:p-1 px-2 md:px-3 bg-gray-800 rounded-r-full w-10 md:w-12"
-                onClick={handleSearch}
-              >
-                🔍
-              </button>
+              <div>
+                <input
+                  className="w-3/4 md:w-1/2 border border-gray-700 md:p-1 pl-2 md:pl-4 rounded-l-full text-black "
+                  type="text"
+                  placeholder="Search"
+                  value={searchData}
+                  onChange={handleSearchDataChange}
+                  onFocus={() => setShowSuggestions(true)}
+                  onBlur={handleOnBlur}
+                />
+                <button
+                  className="border border-gray-700 md:p-1 px-2 md:px-3 bg-gray-800 rounded-r-full w-10 md:w-12"
+                  onClick={handleSearch}
+                >
+                  🔍
+                </button>
               </div>
-            {showSuggestions && <div className="absolute  text-black p-2 w-56">
-              <ul className="bg-white rounded-lg">
-                {storeSuggestions.map((s,index) => <li className="py-1 hover:bg-gray-300 rounded-lg" key={index} onClick={()=>handleSearchSuggestion(s)}>🔍 {s}</li>)}
-              </ul>
-            </div>}
+              {showSuggestions && (
+                <div
+                  className="absolute  text-black m-1 w-1/2 md:w-1/4"
+                  ref={suggestionBoxRef}
+                >
+                  <ul className="bg-white rounded-lg">
+                    {storeSuggestions.map((s, index) => (
+                      <li
+                        className="py-1 hover:bg-gray-300 rounded-lg px-2 my-1"
+                        key={index}
+                        onMouseDown={() => handleSearchSuggestion(s)}
+                      >
+                        🔍 {s}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
             <div className="col-span-4 md:col-span-1 flex">
               <img
